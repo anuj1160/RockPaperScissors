631c496d0727ba074357463a423e59c2a99a4fd7
anuj <anuj.2024cs1160@kiet.edu>
2024-01-31 16:22:55 +0530

    changes occur

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello dosto kya haal chaal sb bdiya</div>;
+  return <div className="App">Hello dosto kya haal chaal sb bdiya koi nhi
+  </div>;
 }
 
 export default App;
