c3d5a5552ed2915ac1b505c514a63d3814480881
mridulcr7 <mridulpandey5277@gmail.com>
2024-01-31 17:11:28 +0530

    changes

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>
-                Welcome Mridul!!
+                Welcome Mridul!
             </h1>
             <div>
                 <label>
