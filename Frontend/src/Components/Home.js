7b70a516bb0f981c9b63f78fd392fd5ce8af1bc7
mridulcr7 <mridulpandey5277@gmail.com>
2024-01-31 16:23:36 +0530

    "Home"

---
+++ Frontend/src/Components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+      <div>
+          <h1>
+              Welcome Mridul!!
+          </h1>
+          <div>
+              <label>
+                  Number of Rounds
+                  <select name="Rounds">
+                      <option value="3">3</option>
+                      <option value="5">5</option>
+                      <option value="7">7</option>
+                  </select>
+              </label>
+          </div>
+         
+    </div>
+  )
+}
